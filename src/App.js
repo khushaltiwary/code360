6d04719327acc0d7e5e13f80d3dfdc154ef18049
khushaltiwary <khushaltiwary@gmail.com>
2025-02-24 14:41:35 +0530

    Created empty file for Login + HomeSc . App working fine.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch , Routes} from 'react-router-dom';
+import LoginSc from './components/LoginSc';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React testing pknkpnkn
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello</h1>
+      <LoginSc/>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/LoginSc.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LoginSc = ()=> {
+    return(
+        <h1>Login Screen</h1>
+    );
+};
+
+export default LoginSc;
