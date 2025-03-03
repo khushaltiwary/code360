6310b5ed22ed95c29a9276523f5181b38bd3137e
khushaltiwary <khushaltiwary@gmail.com>
2025-03-03 16:53:00 +0530

    test by khushal . app woirking fine

--- src/components/LoginSc.js
+++ src/components/LoginSc.js
@@ -3,7 +3,8 @@ import React from "react";
 const LoginSc = ()=> {
     return(
         <h1>Login Screen Demo
-            test commit 
+            test commit
+            test by khushal 
         </h1>
     );
 };
